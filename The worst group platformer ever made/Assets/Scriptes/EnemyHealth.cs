0246b1df1724f09bc749c3a5e23147b533db0cbf
Vicborrayo <128403624+Vicborrayo@users.noreply.github.com>
2023-05-03 09:50:45 -0700

    update

---
+++ The worst group platformer ever made/Assets/Scriptes/EnemyHealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth = 1;
+
+    private void Awake()
+    {
+        currentHealth = 10;
+    } 
+    public void TakeDamage()
+    {
+        currentHealth -= 1;
+    }
+    public void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+}
