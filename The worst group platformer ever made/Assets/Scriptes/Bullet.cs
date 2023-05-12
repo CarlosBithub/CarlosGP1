780bb2e21b805fbfe312c2b207c3b11c22f01224
Vicborrayo <128403624+Vicborrayo@users.noreply.github.com>
2023-05-12 09:05:12 -0700

    update

--- The worst group platformer ever made/Assets/Scriptes/Bullet.cs
+++ The worst group platformer ever made/Assets/Scriptes/Bullet.cs
@@ -7,7 +7,7 @@ public class Bullet : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     BossBehavior bossbehavior;
-    EnemyHeatlh enemyHealth;
+    EnemyHealth enemyHealth;
 
     // Start is called before the first frame update
     void Start()
